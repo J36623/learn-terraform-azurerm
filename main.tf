0928bfd9cdfb0ec1d6147f218f76bdeb3bfc3262
J36623 <nami_nakano@jbcc.co.jp>
2026-07-23 14:03:29 +0900

    PIP関連の情報をコメントアウト

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "azurerm_network_interface" "demo" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.demo.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.demo.id
+//    public_ip_address_id          = azurerm_public_ip.demo.id #PIPの情報をコメントアウト
   }
 }
 

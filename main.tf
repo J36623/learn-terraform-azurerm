9a022d9a7fdf686d4c9577345801f8b29dfd2d2a
Nami Nakano <nami_nakano@jbcc.co.jp>
2026-07-30 12:53:12 +0900

    VMインスタンスタイプ変更1-2
    main.tf
    Standard_D2as_v4  → Standard_D1に変更

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine" "demo" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.demo.name
   location            = azurerm_resource_group.demo.location
-  size                = "Standard_D2as_v4"
+  size                = "Standard_D1"
   admin_username      = var.admin_username
 
   network_interface_ids = [
